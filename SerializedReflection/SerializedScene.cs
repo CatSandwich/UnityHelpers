a0f50df0ffbb621857ae34bbb4b672fec7d6cac5
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2021-09-19 19:48:48 -0400

    Create SerializedScene.cs

---
+++ SerializedReflection/SerializedScene.cs
@@ -0,0 +1,26 @@
+[Serializable]
+public class SerializableScene
+{
+    public Scene Scene => UnityEngine.SceneManagement.SceneManager.GetSceneByName(_scene);
+    [SerializeField] private string _scene;
+}
+
+[CustomPropertyDrawer(typeof(SerializableScene))]
+public class SerializableSceneDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var sceneString = property.FindPropertyRelative("_scene");
+
+        var unselected = "Select...";
+        
+        var arr = Enumerable.Range(0, UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings)
+            .Select(SceneUtility.GetScenePathByBuildIndex)
+            .Select(System.IO.Path.GetFileNameWithoutExtension)
+            .Prepend(unselected)
+            .ToArray();
+        var index = Array.IndexOf(arr, sceneString.stringValue);
+        index = EditorGUI.Popup(position, label.text, index, arr);
+        sceneString.stringValue = index < 0 ? unselected : arr[index];
+    }
+}
